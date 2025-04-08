6eab3a1995fa7d563915abc25c8002bae3c1075b
Manu <manuelcednonr@gmail.com>
2025-04-08 12:27:10 +0200

    Remove badIndex.html and update controlador.php to handle session state and navigation more effectively

--- src/php/clases/controlador.php
+++ src/php/clases/controlador.php
@@ -1,12 +1,46 @@
 <?php
 
-
+require_once 'estado.php';
 require_once 'pinta.php'; 
+
 Class Controlador{
 
-    
-    
+
     public $miEstado;
+
+    function __construct($Estado = null){
+        $this -> miEstado = new Estado();
+        $this -> miEstado -> Estado = 0;
+        $this -> miEstado -> Documentos = array();
+        $this -> miEstado -> FiltrosDoc = array();
+        $this -> miEstado -> acciones = array("archivos"=> 0,
+                                        "observaciones" => 0,
+                                        "añadir" => 0);
+    }
+
+    function navegarPestanas($ps){
+        //volver a la aterior
+        if($ps == -1){
+            //salir del modo formulario
+            
+            if($this -> miEstado -> cargarForm == 1){
+                $this -> miEstado -> cargarForm = 0;
+            }else{
+                $estadoAnterior = array_shift($this -> miEstado -> EstadosAnteriores);
+                $this -> miEstado -> Estado = $estadoAnterior;
+            }
+            //reinicializar variables
+            
+            $this -> miEstado -> nombreDocumentoPadre = null;
+            $this -> miEstado -> IdPropietario = null; 
+        }else{
+            array_unshift($this -> miEstado -> EstadosAnteriores , $this -> miEstado -> Estado);
+            $this -> miEstado -> Estado = $ps;
+        }
+        $this -> miEstado -> CadenaFiltro = null;
+        $this -> miEstado -> IdsTiposFiltro = array();
+        $this -> miEstado -> adjuntarDocumentoFormAutomatico = 0;
+    }
 
     function IniciarSesion($usuario, $contrasena){
         
@@ -34,6 +68,7 @@ Class Controlador{
         $nav = "";
         if($c === 0 && !empty($arrayDatos) && $arrayDatos[0] != -1 && ($this -> miEstado -> tipo_App == 1 || $this -> miEstado -> tipo_App == 2)){
             $nav = 0;
+            echo"$arrayDatos[0] $arrayDatos[1]";
             $InicioS = $this -> IniciarSesion($arrayDatos[0], $arrayDatos[1]);
             if($InicioS ===false){
                 $msgError = "Error de conexión con el servidor, por favor inténtelo más tarde.";
