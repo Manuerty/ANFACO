dd4468a8d1e98ee1e2b6ce3f2502a76d5b1f8de9
Manu <manuelcednonr@gmail.com>
2025-06-02 11:42:59 +0200

    feat: Comment out error logging in controlador.php to reduce output clutter

--- src/php/clases/controlador.php
+++ src/php/clases/controlador.php
@@ -785,7 +785,7 @@ Class Controlador{
 
         $txtErr = "";
 
-        $txtErr = sprintf(
+        /* $txtErr = sprintf(
             "idUsuarioLogIn : %s<br>idUsuarioElegido: %s<br>IdLastUser: %s<br>TagPez: %s<br>LastTagPez: %s<br>Estado: %s<br>IdBoton: %s<br>EstadosAnteriores: %s<br>ArrayDatos: %s",
             $this->miEstado->IdUsuarioLogin,
             $this->miEstado->IdUsuarioSeleccionado,
@@ -796,7 +796,7 @@ Class Controlador{
             $this->miEstado->idBoton,
             implode(",", $this->miEstado->EstadosAnteriores),
             json_encode($arrayDatos, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE)
-        );
+        ); */
 
     
 
